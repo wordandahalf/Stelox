90867d4f40720a6d34b1e9918e10489d6cbc3eb9
Ryan J <mail2ryanjones@gmail.com>
2019-09-30 12:11:57 -0500

    Updated (unused) kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,11 @@
 unsigned char *terminal_buffer = (unsigned char*) 0xB8000;
 
-void main(void)
+int main(void)
 {
     terminal_buffer[2] = 'r';
     terminal_buffer[3] = 0x1B;
 
     for(;;);
+
+    return 0;
 }
