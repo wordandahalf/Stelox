74f404478fe77ba59f4e9a483a5e86518c15f515
Jones <20JonesRy@desmet.org>
2019-10-01 20:28:37 -0500

    Updated stage2
    Switched to standard int sizes
    Started implementation of a read_sector method

--- boot/stage2.c
+++ boot/stage2.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 struct Terminal {
     unsigned short *buffer;
 
@@ -18,12 +20,12 @@ struct Terminal terminal = {
     .height = 25,
 };
 
-static inline int create_vga_color(unsigned int foreground, unsigned int background)
+static inline uint8_t create_vga_color(uint8_t foreground, uint8_t background)
 {
     return foreground | background << 4;
 }
 
-static inline short create_vga_character(unsigned char c, unsigned int color)
+static inline uint16_t create_vga_character(uint8_t c, uint8_t color)
 {
     return (short) c | (short) color << 8;
 }
@@ -40,7 +42,7 @@ static void terminal_init()
     terminal.buffer = (unsigned short*) 0xB8000;
 }
 
-static void putch(unsigned char ch)
+static void putch(uint8_t ch)
 {
     if(++terminal.selected_column == terminal.width)
     {
@@ -61,11 +63,57 @@ static void puts(char *str)
         putch(str[i]);
 }
 
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    unsigned char ret;
+
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+
+    return ret;
+}
+
+static void read_sector(uint32_t lba, uint8_t sectors, int64_t buffer_address)
+{
+    uint8_t address = (uint8_t) ((lba >> 24) | 0b11100000);
+
+    outb(0x01F6, address); // Send bits 24-27 of the LBA for first sector
+    outb(0x01F2, sectors); // Send number of sectors
+    outb(0x01F3, (uint8_t) (lba & 0xF)); // Send bits 0-7 of the LBA
+    outb(0x01F4, (uint8_t) ((lba >> 8) & 0xF)); // Send bits 8-15 of the LBA
+    outb(0x01F5, (uint8_t) ((lba >> 16) & 0xF)); // Send bits 16-23 of the LBA
+
+    outb(0x01F7, 0x20); // Command for read w/ retry
+
+    while(!(0x8 & inb(0x01F7))) {}
+
+    for(int i = 0; i < 255; i++)
+    {
+        //Input word from I/O port specified in DX into memory location specified in ES:(E)DI or RDI.
+    }    
+
+    /*
+               mov rax, 256         ; to read 256 words = 1 sector
+               xor bx, bx           ; bx = 0
+               mov bl, cl           ; read CL sectors
+               mul bx               ; dx:ax = ax * bx
+               mov rcx, rax         ; RCX is counter for INSW
+               mov rdx, 0x1F0       ; Data port, in and out
+               rep insw             ; in to [RDI]
+    */
+}
+
 int kmain(void)
 {
     terminal_init();
 
     puts("Welcome to Stelox v0.0.1!");
+
+    read_sector(0, 1, 0x7E00);
 
     for(;;);
 
