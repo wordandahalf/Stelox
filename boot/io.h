7dcd8415e6dfa28bdcec88f34464cf5e68ff5d29
Jones <20JonesRy@desmet.org>
2019-10-16 22:58:17 -0500

    Added more I/O methods

--- boot/io.h
+++ boot/io.h
@@ -8,6 +8,11 @@ inline void outb(uint16_t port, uint8_t val)
     asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
+inline void outw(uint16_t port, uint16_t val)
+{
+    asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
 inline uint8_t inb(uint16_t port)
 {
     uint8_t ret = 0;
@@ -17,12 +22,13 @@ inline uint8_t inb(uint16_t port)
     return ret;
 }
 
-inline void inportsm(uint16_t port, uint8_t *data, uint64_t size) {
-	asm volatile ("rep insw" : "+D" (data), "+c" (size) : "d" (port) : "memory");
-}
+inline uint16_t inw(uint16_t port)
+{
+    uint16_t ret = 0;
 
-inline void outportsm(uint16_t port, uint8_t *data, uint64_t size) {
-	asm volatile ("rep outsw" : "+S" (data), "+c" (size) : "d" (port));
+    asm volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+
+    return ret;
 }
 
 #endif
