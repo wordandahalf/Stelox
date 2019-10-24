bca2d37e1825ed627376598a77230ad7df254a99
Jones <20JonesRy@desmet.org>
2019-10-24 16:06:21 -0500

    Barebones UEFI loader
    Simply prints "Hello, world!" to the screen.
    Uses clang and lld, soon to change to the usual gcc and ld.
    
    Co-Authored-By: Ryan J. <mail2ryanjones@gmail.com>

---
+++ kernel/kernel.d
@@ -0,0 +1,6 @@
+kernel/kernel.o: kernel/kernel.c kernel/include/efi/boot-services.h \
+  kernel/include/efi/types.h kernel/include/efi/protocol/device-path.h \
+  kernel/include/efi/runtime-services.h \
+  kernel/include/efi/system-table.h \
+  kernel/include/efi/protocol/simple-text-input.h \
+  kernel/include/efi/protocol/simple-text-output.h
