6582e99c2e6df5d88eaa7ae4ee0650318ffa7b66
wordandahalf <mail2ryanjones@gmail.com>
2019-11-07 10:21:02 -0600

    Started work on ELF loader

--- boot/uefi/include/io.h
+++ boot/uefi/include/io.h
@@ -1,14 +1,18 @@
 #ifndef __UEFI_IO_H_
 #define __UEFI_IO_H_
+
 #include <efi.h>
 #include <efilib.h>
 
-void check_efi_error(EFI_STATUS status, const CHAR16 *error_msg)
+BOOLEAN check_efi_error(EFI_STATUS status, const CHAR16 *error_msg)
 {
     if(EFI_ERROR(status))
     {
         Print(L"[%EERROR%N] %H%s%N\r\n", error_msg);
+        return TRUE;
     }
+
+    return FALSE;
 }
 
 void log_msg(const CHAR16 *msg)
@@ -25,6 +29,18 @@ void log_info(const CHAR16 *msg)
     Print(L"INFO");
     uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, 0x0F);
     Print(L"] %s%N\r\n", msg);
+}
+
+EFI_STATUS load_kernel(EFI_SYSTEM_TABLE *ST, EFI_HANDLE image_handle, /* GPU_CONFIG * Graphics, */ EFI_CONFIGURATION_TABLE *config_tables, UINTN number_of_config_tables, UINT32 uefi_version)
+{
+    EFI_STATUS status;
+
+    EFI_PHYSICAL_ADDRESS kernel_base_address = 0;
+    UINTN kernel_pages = 0;
+
+    EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
+
+    return EFI_SUCCESS;
 }
 
 #endif
