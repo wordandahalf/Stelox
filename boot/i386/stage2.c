2c6e8118ab890e4376170151a5b381097ed9fc2c
wordandahalf <mail2ryanjones@gmail.com>
2020-02-08 14:16:10 -0600

    Redid the stage2 loader

--- boot/i386/stage2.c
+++ boot/i386/stage2.c
@@ -1,29 +1,53 @@
-#include <stdint.h>
-#include <stdbool.h>
+#include <stdarg.h>
 
+#include "types.h"
+#include "io.h"
 #include "terminal.h"
 #include "ata.h"
 
-uint16_t *KERNEL_ADDRESS = (uint16_t*)0x7E00;
+uint8_t *KERNEL_ADDRESS = (uint8_t*)0x0500;
+
+void read_sectors(AtaDevice *device)
+{
+    atapi_read_sector(device, 0x11, 1, KERNEL_ADDRESS);
+}
 
 int loader_main(void)
 {
     terminal_init();
 
-    printf("Welcome to %<Stelox%@ v0.0.1!\n", create_vga_color(0x3, 0x0));
+    log("Stelox v0.0.1 booted!", TERMINAL_INFO_LOG);
 
-    AtaDevice *device = ata_find_devices();
-    ata_select_device(device, -1);
+    ata_detect_devices();
 
-    if(device)
+    for(int i = 0; i < 1024; i++)
+        KERNEL_ADDRESS[i] = 0xFF;
+
+    if(ata_primary_master.type == ATA_PATAPI_DRIVE)
     {
-        printf("Valid ATA device found...\nType: %x\n", (*device).type);
-        ata_read_sector(0, 1, KERNEL_ADDRESS);
-        printf("Done reading to %x\n", KERNEL_ADDRESS);
+        read_sectors(&ata_primary_master);
     }
-    else
+    if(ata_primary_slave.type == ATA_PATAPI_DRIVE)
     {
-        put_string("No valid ATA device found!\nPlease try restarting...\n");
+        read_sectors(&ata_primary_slave);
+    }
+    if(ata_secondary_master.type == ATA_PATAPI_DRIVE)
+    {
+        read_sectors(&ata_secondary_master);
+    }
+    if(ata_secondary_slave.type == ATA_PATAPI_DRIVE)
+    {
+        read_sectors(&ata_secondary_slave);
+    }
+
+    for(int y = 0; y < 16; y++)
+    {
+        for(int x = 0; x < 16; x++)
+        {
+            put_int(KERNEL_ADDRESS[(y * 16) + x], false, 16);
+            printf(" ");
+        }
+        printf("\n");
     }
 
     for(;;) {}
