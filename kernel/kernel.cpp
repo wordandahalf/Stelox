fb8260ed9ab9689a7cfab2eebfaa0cf08f22dad2
wordandahalf <mail2ryanjones@gmail.com>
2020-02-15 21:49:10 -0600

    Added bare-bones higher-half C++ kernel. Thanks OSDev Wiki!

---
+++ kernel/kernel.cpp
@@ -0,0 +1,19 @@
+unsigned short *terminal_buffer = (unsigned short*) 0xC03FF000;
+
+// Top-of-the-line print function
+void prints(const char *msg)
+{
+    for(unsigned int i = 0; *msg; i++)
+    {
+        terminal_buffer[i] =  0x0F << 8 | *msg; // Color + character (little endian)
+        msg++;
+    }
+}
+
+extern "C"
+void kernel_main(void)
+{
+    prints("Hello, kernel-dev!");
+
+    for(;;);
+}
