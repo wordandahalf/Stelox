ea02a8d7057e2ba7f1a3913c95d7f5961f14eb01
Ryan Jones <10679776+wordandahalf@users.noreply.github.com>
2026-07-02 10:45:24 -0400

    change: compile in debug mode, for now

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const x86 = std.Target.x86;
 
 // todo: probably don't need this, all x86_64 chips likely have these
 const disabled_features = x86.featureSet(&[_]x86.Feature{ .mmx, .sse, .sse2, .avx, .avx2 });
-const enabled_features = x86.featureSet(&[_]x86.Feature{ .soft_float });
+const enabled_features = x86.featureSet(&[_]x86.Feature{.soft_float});
 
 const bootloader_target = std.Target.Query{
     .cpu_arch = .x86_64,
@@ -22,17 +22,17 @@ const kernel_target = std.Target.Query{
 };
 
 pub fn build(b: *std.Build) void {
-    const mode = b.standardOptimizeOption(.{});
+    const mode = b.standardOptimizeOption(.{ .preferred_optimize_mode = .Debug });
 
     const lib_mod = b.addModule("lib", .{
-        .root_source_file = b.path("lib/root.zig")
+        .root_source_file = b.path("lib/root.zig"),
     });
     lib_mod.addImport("lib", lib_mod);
 
     const bootloader_mod = b.addModule("bootloader", .{
         .root_source_file = b.path("src/boot/main.zig"),
         .target = b.resolveTargetQuery(bootloader_target),
-        .optimize = mode
+        .optimize = mode,
     });
     bootloader_mod.addImport("lib", lib_mod);
 
@@ -40,20 +40,20 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/kernel/main.zig"),
         .target = b.resolveTargetQuery(kernel_target),
         .optimize = mode,
-        .code_model = .kernel
+        .code_model = .kernel,
     });
     kernel_mod.addImport("lib", lib_mod);
 
     const bootloader_exe = b.addExecutable(.{
         .name = "bootx64",
-        .root_module = bootloader_mod
+        .root_module = bootloader_mod,
     });
 
     b.installArtifact(bootloader_exe);
 
     const kernel_exe = b.addExecutable(.{
         .name = "kernel.elf",
-        .root_module = kernel_mod
+        .root_module = kernel_mod,
     });
 
     kernel_exe.use_llvm = true;
@@ -67,13 +67,13 @@ pub fn build(b: *std.Build) void {
     var kernel_step = b.step("kernel", "Build kernel");
     kernel_step.dependOn(&kernel_exe.step);
 
-    var create_image = b.addSystemCommand(&.{ "./utils/make-fat.sh" });
+    var create_image = b.addSystemCommand(&.{"./utils/make-fat.sh"});
     create_image.addArg("-i");
     create_image.addArtifactArg(bootloader_exe);
     create_image.addArg("-o");
     const created_image = create_image.addOutputFileArg("bootloader.fat");
 
-    var create_iso = b.addSystemCommand(&.{ "xorriso" });
+    var create_iso = b.addSystemCommand(&.{"xorriso"});
     create_iso.addArgs(&.{ "-as", "mkisofs", "-f", "-e", "efi/boot/efi.fat", "-no-emul-boot" });
     create_iso.addArg("-o");
     const created_iso = create_iso.addOutputFileArg("stelox.iso");
@@ -84,7 +84,7 @@ pub fn build(b: *std.Build) void {
     const installed_file = b.addInstallFileWithDir(created_iso, .{ .custom = "." }, "stelox.iso");
     b.getInstallStep().dependOn(&installed_file.step);
 
-    const run_iso = b.addSystemCommand(&.{ "qemu-system-x86_64" });
+    const run_iso = b.addSystemCommand(&.{"qemu-system-x86_64"});
     run_iso.addArgs(&.{ "-drive", "if=pflash,format=raw,file=OVMF.fd" });
     run_iso.addArg("-cdrom");
     run_iso.addFileArg(created_iso);
